fd3cd147769ffcc8c001a3d24d49e3cf5ca73403
Rolin Blake <rolinmblake@gmail.com>
2023-10-19 06:56:21 -0500

    now uses float64 and inverses work properly

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 type Matrix struct {
   m int
   n int
-  mat [][]int
+  mat [][]float64
 }
 
 func printMatrix(mtrx Matrix) {
@@ -20,7 +20,7 @@ func printMatrix(mtrx Matrix) {
   fmt.Println("]")
 }
 
-func makeMatrix(data [][]int) (Matrix, error) {
+func makeMatrix(data [][]float64) (Matrix, error) {
   var mRows int
   var nCols int
   if len(data) >= 1 {
@@ -51,9 +51,9 @@ func matrixAdd(a,b Matrix) (Matrix, error) {
   if a.m != b.m || a.n != b.n {
     return Matrix{}, errors.New("\n[ERROR] Can only add Matrix objects of same MxN dimensions")
   } else {
-    sum := make([][]int, a.m)
+    sum := make([][]float64, a.m)
     for i := 0; i < a.m; i++ {
-      sum[i] = make([]int, a.n)
+      sum[i] = make([]float64, a.n)
       for j := 0; j < a.n; j++ {
         sum[i][j] = a.mat[i][j] + b.mat[i][j]
       }
@@ -70,9 +70,9 @@ func matrixSub(a,b Matrix) (Matrix, error) {
   if a.m != b.m || a.n != b.n {
     return Matrix{}, errors.New("\n[ERROR] Can only subtract Matrix objects of same MxN dimensions")
   } else {
-    dif := make([][]int, a.m)
+    dif := make([][]float64, a.m)
     for i := 0; i < a.m; i++ {
-      dif[i] = make([]int, a.n)
+      dif[i] = make([]float64, a.n)
       for j := 0; j < a.n; j++ {
         dif[i][j] = a.mat[i][j] - b.mat[i][j]
       }
@@ -89,10 +89,11 @@ func matrixMult(a,b Matrix) (Matrix, error) {
   if a.n != b.m {
     return Matrix{}, errors.New("\n[ERROR] Incompatible matrix dimensions for multiplication")
   }
-  result := make([][]int, a.m)
+  result := make([][]float64, a.m)
   for i := 0; i < a.m; i++ {
-    result[i] = make([]int, b.n)
+    result[i] = make([]float64, b.n)
     for j := 0; j < b.n; j++ {
+      result[i][j] = 0.0
       for k := 0; k < a.n; k++ {
 	      result[i][j] += a.mat[i][k] * b.mat[k][j]
       }
@@ -106,9 +107,9 @@ func matrixMult(a,b Matrix) (Matrix, error) {
 }
 
 func subMatrix(mtrx Matrix, i,j int) Matrix {
-  sub := make([][]int, mtrx.m-1)
+  sub := make([][]float64, mtrx.m-1)
 	for k := range sub {
-		sub[k] = make([]int, mtrx.m-1)
+		sub[k] = make([]float64, mtrx.m-1)
 	}
 	row := 0
 	for r := 0; r < mtrx.m; r++ {
@@ -132,7 +133,7 @@ func subMatrix(mtrx Matrix, i,j int) Matrix {
   }
 }
 
-func matrixDet(mtrx Matrix) (int, error) {
+func matrixDet(mtrx Matrix) (float64, error) {
   if mtrx.m != mtrx.n {
     return 0, errors.New("Cannot find determinant of a non-square Matrix")  
   }
@@ -145,13 +146,13 @@ func matrixDet(mtrx Matrix) (int, error) {
   if mtrx.m == 3 {
     return mtrx.mat[0][0]*mtrx.mat[1][1]*mtrx.mat[2][2] + mtrx.mat[0][1]*mtrx.mat[1][2]*mtrx.mat[2][0] + mtrx.mat[0][2]*mtrx.mat[1][0]*mtrx.mat[2][1] - mtrx.mat[0][2]*mtrx.mat[1][1]*mtrx.mat[2][0] - mtrx.mat[0][0]*mtrx.mat[1][2]*mtrx.mat[2][1] - mtrx.mat[0][1]*mtrx.mat[1][0]*mtrx.mat[2][2], nil
   }
-  det := 0
-  sign := 1
+  det := 0.0
+  sign := 1.0
   for j := 0; j < mtrx.m; j++ {
     subMtrx := subMatrix(mtrx, 0, j)
     subDet, _ := matrixDet(subMtrx)
     det += sign * mtrx.mat[0][j] * subDet
-    sign = -sign
+    sign *= -1.0
   }
   return det, nil
 }
@@ -174,25 +175,28 @@ func matrixInv(mtrx Matrix) (Matrix, error) {
   if !hasInverse(mtrx) {
     return Matrix{}, errors.New("Cannot find the inverse of supplied matrix")
   }
-  id := make([][]int, mtrx.m)
+  id := make([][]float64, mtrx.m)
+  copyMat := make([][]float64, mtrx.m)
   for i := 0; i < mtrx.m; i++ {
-    id[i] = make([]int, mtrx.m)
+    id[i] = make([]float64, mtrx.m)
     id[i][i] = 1
+    copyMat[i] = make([]float64, mtrx.n)
+    copy(copyMat[i], mtrx.mat[i])
   }
   for i := 0; i < mtrx.m; i++ {
-    if mtrx.mat[i][i] == 0 {
+    if copyMat[i][i] == 0 {
       return Matrix{}, errors.New("Matrix is singular, cannot find its inverse")
     }
-    div := mtrx.mat[i][i]
+    div := copyMat[i][i]
     for j := 0; j < mtrx.m; j++ {
-      mtrx.mat[i][j] /= div
+      copyMat[i][j] /= div
       id[i][j] /= div
     }
     for k := 0; k < mtrx.m; k ++ {
       if k != i {
-        mul := mtrx.mat[k][i]
+        mul := copyMat[k][i]
         for l := 0; l < mtrx.m; l++ {
-          mtrx.mat[k][l] -= mul * mtrx.mat[i][l]
+          copyMat[k][l] -= mul * copyMat[i][l]
           id[k][l] -= mul * id[i][l]
         }
       }
@@ -208,13 +212,14 @@ func matrixInv(mtrx Matrix) (Matrix, error) {
 
 func runMatrixTest() {
   testStart := time.Now()
-  data := [][]int{
-    {0, 1},
-    {0, 1},
-    {0, 1},
-    {0, 1},
-    {0, 1},
-    {0, 1, 2, 3},
+  // TESTS ON INVALID STRUCTS
+  data := [][]float64 {
+    {0.0, 1.0},
+    {0.0, 1.0},
+    {0.0, 1.0},
+    {0.0, 1.0},
+    {0.0, 1.0},
+    {0.0, 1.0, 2.0, 3.0},
   }
   badMat, err := makeMatrix(data)
   if err != nil {
@@ -222,10 +227,10 @@ func runMatrixTest() {
     fmt.Println("\nInvalid Matrix ", data)
     fmt.Println("\nReturned Matrix{}: ", badMat)
   }
-  data = [][]int {
+  data = [][]float64 {
     {},
-    {0},
-    {1},
+    {0.0},
+    {1.0},
   }
   badMat, err = makeMatrix(data)
   if err != nil {
@@ -233,10 +238,10 @@ func runMatrixTest() {
     fmt.Println("\nInvalid Matrix ", data)
     fmt.Println("\nReturned Matrix{}: ", badMat)
   }
-  data = [][]int {
-    {0, 1},
+  data = [][]float64 {
+    {0.0, 1.0},
     {},
-    {0, 1},
+    {0.0, 1.0},
   }
   badMat, err = makeMatrix(data)
   if err != nil {
@@ -244,121 +249,156 @@ func runMatrixTest() {
     fmt.Println("\nInvalid Matrix ", data)
     fmt.Println("\nReturned Matrix{}: ", badMat)
   }
-  data = [][]int {
-    {0},
-  }
-  goodMat, err := makeMatrix(data)
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println("\nValid Matrix{}: ", goodMat)
-  printMatrix(goodMat)  
-  data = [][]int{
-    {0, 1, 2, 3},
-    {4, 5, 6, 7},
-    {8, 9, 10, 11},
-  }
-  goodMat, err = makeMatrix(data)
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println("\nValid Matrix{} (A): ", goodMat)
-  printMatrix(goodMat)
-  data0 := [][]int {
-    {-1, -1, -1, -1},
-    {-1, -1, -1, -1},
-    {-1, -1, -1, -1},
+  // TESTS ON VALID STRUCTS
+  data0 := [][]float64 {
+    {0.0},
   }
   goodMat0, err := makeMatrix(data0)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Matrix{} (B): ", goodMat0)
-  printMatrix(goodMat0)
-  sumMat, err := matrixAdd(goodMat, goodMat0)
+  fmt.Println("\nValid Matrix{}: ", goodMat0)
+  printMatrix(goodMat0)  
+  dataA := [][]float64 {
+    {0.0, 1.0, 2.0, 3.0},
+    {4.0, 5.0, 6.0, 7.0},
+    {8.0, 9.0, 10.0, 11.0},
+  }
+  goodMatA, err := makeMatrix(dataA)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nValid Matrix{} (A): ", goodMatA)
+  printMatrix(goodMatA)
+  dataB := [][]float64 {
+    {-1.0, -1.0, -1.0, -1.0},
+    {-1.0, -1.0, -1.0, -1.0},
+    {-1.0, -1.0, -1.0, -1.0},
+  }
+  goodMatB, err := makeMatrix(dataB)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nValid Matrix{} (B): ", goodMatB)
+  printMatrix(goodMatB)
+  sumMat, err := matrixAdd(goodMatA, goodMatB)
   if err != nil {
     fmt.Println(err)
   }
   fmt.Println("\nValid Sum Result 1 (A+B): ", sumMat)
   printMatrix(sumMat)
-  sumMat, err = matrixAdd(goodMat0, goodMat)
+  sumMat, err = matrixAdd(goodMatB, goodMatA)
   if err != nil {
     fmt.Println(err)
   }
   fmt.Println("\nValid Sum Result 2 (B+A): ", sumMat)
   printMatrix(sumMat)
-  difMat, err := matrixSub(goodMat, goodMat0)
+  difMatAB, err := matrixSub(goodMatA, goodMatB)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Difference Result 1 (A-B): ", difMat)
-  printMatrix(difMat)
-  difMat0, err := matrixSub(goodMat0, goodMat)
+  fmt.Println("\nValid Difference Result 1 (A-B): ", difMatAB)
+  printMatrix(difMatAB)
+  difMatBA, err := matrixSub(goodMatB, goodMatA)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Difference Result 2 (B-A): ", difMat0)
-  printMatrix(difMat0)
-  data1 := [][]int {
-    {0, 1, 2},
-    {3, 4, 5},
+  fmt.Println("\nValid Difference Result 2 (B-A): ", difMatBA)
+  printMatrix(difMatBA)
+  dataC := [][]float64 {
+    {0.0, 1.0, 2.0},
+    {3.0, 4.0, 5.0},
   }
-  data2 := [][]int {
-    {0, 1},
-    {2, 3},
-    {4, 5},
+  dataD := [][]float64 {
+    {0.0, 1.0},
+    {2.0, 3.0},
+    {4.0, 5.0},
   }
-  goodMat1, err := makeMatrix(data1)
+  goodMatC, err := makeMatrix(dataC)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Matrix{} (C): ", goodMat1)
-  printMatrix(goodMat1)
-  goodMat2, err := makeMatrix(data2)
+  fmt.Println("\nValid Matrix{} (C): ", goodMatC)
+  printMatrix(goodMatC)
+  goodMatD, err := makeMatrix(dataD)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Matrix{} struct (D): ", goodMat2)
-  multMat, err := matrixMult(goodMat1, goodMat2)
+  fmt.Println("\nValid Matrix{} (D): ", goodMatD)
+  printMatrix(goodMatD)
+  multMatCD, err := matrixMult(goodMatC, goodMatD)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Multiplication Result (C*D): ", multMat)
-  data3 := [][]int {
-    {2, 4, 1, 3, 5},
-		{5, 6, 2, 7, 2},
-		{1, 2, 6, 3, 1},
-		{3, 1, 3, 6, 2},
-    {2, 2, 2, 2, 2},
-  }
-  goodMat3, err := makeMatrix(data3)
+  fmt.Println("\nValid Multiplication Result (C*D): ", multMatCD)
+  printMatrix(multMatCD)
+  multMatDC, err := matrixMult(goodMatD, goodMatC)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nValid Matrix{} (E): ", goodMat3)
-  printMatrix(goodMat3)
-  detTest, err := matrixDet(goodMat3)
+  fmt.Println("\nValid Multiplication Result (D*C): ", multMatDC)
+  printMatrix(multMatDC)
+  dataE := [][]float64 {
+    {4.0, 3.0},
+    {3.0, 2.0},
+  }
+  goodMatE, err := makeMatrix(dataE)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nMatrix E Determinant: ", detTest)
-  invMat, err := matrixInv(goodMat3)
+  fmt.Println("\nValid Matrix{} (E): ", goodMatE)
+  printMatrix(goodMatE)
+  detTestE, err := matrixDet(goodMatE)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nInverse of Matrix E: ", invMat)
-  printMatrix(invMat)
-  testInvMult, err := matrixMult(goodMat3, invMat)
+  fmt.Println("\nMatrix E Determinant: ", detTestE)
+  invMatE, err := matrixInv(goodMatE)
   if err != nil {
     fmt.Println(err)
   }
-  fmt.Println("\nProduct of E * Inverse of E: ", testInvMult)
-  printMatrix(testInvMult)
+  fmt.Println("\nInverse of Matrix E: ", invMatE)
+  printMatrix(invMatE)
+  shouldBeId, err := matrixMult(goodMatE, invMatE)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nProduct of E * Inverse of E: ", shouldBeId)
+  printMatrix(shouldBeId)
+  dataF := [][]float64 {
+    {7.0, -3.0, -3.0},
+    {-1.0, 1.0, 0.0},
+    {-1.0, 0.0, 1.0},
+  }
+  goodMatF, err := makeMatrix(dataF)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nValid Matrix{} (F): ", goodMatF)
+  printMatrix(goodMatF)
+  detTestF, err := matrixDet(goodMatF)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nMatrix F Determinant: ", detTestF)
+  invMatF, err := matrixInv(goodMatF)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nInverse of Matrix F: ", invMatF)
+  printMatrix(invMatF)
+  shouldBeId, err = matrixMult(goodMatF, invMatF)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("\nProduct of F * Inverse of F: ", shouldBeId)
+  printMatrix(shouldBeId)
+  // END OF TESTS
   testEnd := time.Now()
   testTime := testEnd.Sub(testStart).Nanoseconds()
   fmt.Printf("runMatrixTest() Execution Time: %d nanoseconds", testTime)
 }
-
+// MAIN EXECUTION
 func main() {
   runMatrixTest()
 }
